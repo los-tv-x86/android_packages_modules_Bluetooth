6ffedb75fdb22f4b1ff5aadff0430ddc7131b60d
Chih-Wei Huang <cwhuang@linux.org.tw>
2019-05-30 10:50:19 +0800

    hciblecmds: remove unnecessary checking
    This fixes Bluetooth USB dongle support.
    
    Change-Id: Ib224d22e20756cb325f546d87bd7d348564a3b0b

--- system/stack/hcic/hciblecmds.cc
+++ system/stack/hcic/hciblecmds.cc
@@ -23,6 +23,8 @@
  *
  ******************************************************************************/
 
+#define LOG_TAG "bt_hciblecmds"
+
 #include <base/functional/bind.h>
 #include <bluetooth/log.h>
 #include <bluetooth/types/address.h>
@@ -163,8 +165,9 @@ void btsnd_hcic_ble_rand(base::Callback<void(BT_OCTET8)> cb) {
           HCI_BLE_RAND, nullptr, 0,
           base::Bind(
                   [](base::Callback<void(BT_OCTET8)> cb, uint8_t* param, uint16_t /* param_len */) {
-                    bluetooth::log::assert_that(param[0] == 0,
-                                                "LE Rand return status must be zero");
+                    if (param[0] != 0) {
+                      bluetooth::log::warn("LE Rand return status is not zero: {}", param[0]);
+                    }
                     cb.Run(param + 1 /* skip status */);
                   },
                   std::move(cb)));
